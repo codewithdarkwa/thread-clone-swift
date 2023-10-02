9f7d873a5a5357706ae239f633bd7ec44b48b050
codewithdarkwa <jnrdrkw1@gmail.com>
2023-10-02 18:50:13 +0000

    Initial Commit

---
+++ thread-clone/thread_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  thread_cloneApp.swift
+//  thread-clone
+//
+//  Created by Darkwa John on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct thread_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
